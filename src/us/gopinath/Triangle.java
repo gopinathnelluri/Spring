ca634a8820522a070f5883f3a739f5ca7b58f84d
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2017-07-22 04:25:51 -0500

    autowiring by name

--- src/us/gopinath/Triangle.java
+++ src/us/gopinath/Triangle.java
@@ -1,29 +1,38 @@
 package us.gopinath;
 
-import java.util.List;
-
 public class Triangle {
 	
-	private List<Point> points;
-
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
-
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
-
-
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
+	public Point getPointB() {
+		return pointB;
+	}
 
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
 
 	public void draw() {
-		for (Point point: points) {
-			System.out.println("point = ("+point.getX()+","+point.getY()+")");
-		}
-		
+		System.out.println("point A = ("+getPointA().getX()+","+getPointA().getY()+")");
+		System.out.println("point B = ("+getPointB().getX()+","+getPointB().getY()+")");
+		System.out.println("point C = ("+getPointC().getX()+","+getPointC().getY()+")");
 	}
 }
