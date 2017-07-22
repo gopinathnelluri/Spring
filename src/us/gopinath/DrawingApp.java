688e78a2133335ec8931fca0fd724b6e768cdf84
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2017-07-22 02:28:26 -0500

    Dependency Injection of objects using xml

---
+++ src/us/gopinath/DrawingApp.java
@@ -0,0 +1,22 @@
+package us.gopinath;
+
+import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.core.io.FileSystemResource;
+
+@SuppressWarnings("deprecation")
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Triangle triangle = new Triangle();
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
