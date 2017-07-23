76d3bd345717d2858f44b03737d8ef0823f5b8ff
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2017-07-22 21:08:42 -0500

    @Autowire Annotations with Qualifier

--- src/us/gopinath/Circle.java
+++ src/us/gopinath/Circle.java
@@ -1,6 +1,9 @@
 package us.gopinath;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+//import org.springframework.beans.factory.annotation.Required;
 
 public class Circle implements Shape{
 
@@ -10,7 +13,8 @@ public class Circle implements Shape{
 		return center;
 	}
 
-	@Required
+	@Autowired
+	@Qualifier("circleRelated")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
